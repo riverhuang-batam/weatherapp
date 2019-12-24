a691a0fe87dca333b149a75e3724ce82631f578a
riverhuang-batam <riverhuang011@gmail.com>
2019-12-24 13:33:33 +0700

    mt-4 on temp

--- src/weather/index.js
+++ src/weather/index.js
@@ -18,10 +18,11 @@ export default class Weather extends Component{
             <div style={{color:"white"}}>
                 
                 <i className={`wi ${this.props.icon} display-1 mt-4`} /> 
+                <div className="mt-4">
                {this.props.temp_celsius ? (<h1>{this.props.temp_celsius}&deg;</h1>) : null}
                
                 {this.minmaxTemp(this.props.temp_min, this.props.temp_max)}
-                
+                </div>
                 <h1>{this.props.description}</h1>
             </div>
         )
